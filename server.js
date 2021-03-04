8b539eec94ea2b17fe04d0173f6052a94df06058
Justin Hohing <jnh0627@yahoo.com>
2021-03-03 20:46:34 -0500

    add the post and delete methods

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Set up for the Express App
-var app = express();
-var port = 1021;
-var mainDir = path.join(__dirname, "/public");
+const app = express();
+const port = 1021;
+const mainDir = path.join(__dirname, "/public");
 
 // Sets up the Express app to handle data parsing
 app.use(express.static("public"));
@@ -15,22 +15,54 @@ app.use(express.json());
 
 //Get methods to grab all the required pages and the data to be displayed
 
-// app.get("/notes", function(req, res){
-//     res.sendFile(path.join(mainDir, "notes.html"));
-// });
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
 
-// app.get("*", function(req, res){
-//     res.sendFile(path.join(mainDir, "index.html"));
-// });
+app.get("*", function(req, res){
+    res.sendFile(path.join(mainDir, "index.html"));
+});
 
-// app.get("/api/notes", function(req, res){
-//     res.sendFile(path.join(__dirname, "/db/db.json"));
-// });
+app.get("/api/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-// app.get("/api/notes/:id", function(req, res){
-//     var savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-//     res.json(savedNotes[Number(req.params.id)]);
-// });
+app.get("/api/notes/:id", function(req, res){
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.post("/api/notes", function(req, res){
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db/json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("New note has been saved to db.json. Here is what the note says: ", newNote);
+    res.json(savedNotes);
+});
+
+app.delete("api/notes/:id", function(req, res){
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+
+    console.log(`Deleting note with following ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    });
+
+    for(currNote of savedNotes){
+        currNote.id = newID.toString();
+        newID++;
+    };
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
 
 app.listen(port, () => {
     console.log("Now listening to https://localhost:" + port);
